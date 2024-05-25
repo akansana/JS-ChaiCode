f06666fe3a2e00be93ac17fdd9c81541298d24e8
akansana <100847609+akansana@users.noreply.github.com>
2024-05-25 18:06:30 +0000

    Completed; variable, DT, converstionOperation

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 23456
+let accountEmail = "ak@gmail.com"
+var accoundPassword = "12345" // Allowed but don't use
+accountCity = "Gwalior" // Allowed but don't use it
+
+/*
+Prefer not to use Var
+Because of block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId, accountEmail, accoundPassword, accountCity])
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # JS-ChaiCode
+#Steps  (01_Basics): 
+1. Create a github repository; go to codespace.
+2. Config devcontainer; click on container; nodejs + JS
+3. Build
+4. Fork;Commit & Push.
+5. Once done, Deactivate the codespace.
+#Steps  (01_Variables): 
